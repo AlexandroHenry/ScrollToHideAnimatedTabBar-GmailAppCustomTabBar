32ba09c08e4e3b8d6527453a1bc5989e8fb69271
alaxhenry <alaxhenry0121@gmail.com>
2022-12-12 18:10:12 +0900

    Initial Commit

---
+++ ScrollToHideAnimatedTabBar-GmailAppCustomTabBar/ScrollToHideAnimatedTabBar_GmailAppCustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollToHideAnimatedTabBar_GmailAppCustomTabBarApp.swift
+//  ScrollToHideAnimatedTabBar-GmailAppCustomTabBar
+//
+//  Created by Seungchul Ha on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct ScrollToHideAnimatedTabBar_GmailAppCustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
